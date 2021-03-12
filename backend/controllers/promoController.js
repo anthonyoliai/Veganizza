5aa968865caa6628aaf41afffaa3474f63147841
unknown <anthonyoliai@gmail.com>
2021-03-13 00:51:02 +0100

    Fixes to remaining models

--- backend/controllers/promoController.js
+++ backend/controllers/promoController.js
@@ -1,5 +1,5 @@
+import Promo from '../models/promoModel.js'
 import asyncHandler from 'express-async-handler'
-import Promo from '../models/PromoModel.js'
 
 const getPromos = asyncHandler(async (req, res) => {
   const promos = await Promo.find({})
