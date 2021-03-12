a404f5c0c1db8b2f74490bc647f4a86073a90910
unknown <anthonyoliai@gmail.com>
2021-03-13 00:46:48 +0100

    Model name fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
+import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
-import Product from '../models/ProductModel.js'
 
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({})
